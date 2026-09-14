eda7aa984a9188c3be5da60604db655077584d3e
ANafeesh <ahamednafeesh2003@gmail.com>
2026-09-14 15:13:15 +0530

    Set up routing and payment module page skeleton

---
+++ Smart Hostel and Co-Living/src/pages/student/BillDashboard.jsx
@@ -0,0 +1,10 @@
+function BillDashboard() {
+  return (
+    <div className="card">
+      <h2>My Bills</h2>
+      <p>Bill list will appear here.</p>
+    </div>
+  );
+}
+
+export default BillDashboard;
